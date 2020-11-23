5af538b0f3177cee927ede95b4d69235335e6495
clintonhays <clintonhays84@gmail.com>
2020-11-24 07:50:27 +0900

    add placeholder styling

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Quote from './Quote';
 
-class QuoteBox extends Component {
-  render () {
-    const { quote, author } = this.props;
-    return (
-      <div id="quote-box">
-        <Quote quote={quote} author={author} />
-      </div>
-    );
-  }
-}
+const QuoteBox = ({ quote, author }) => {
+  return (
+    <div id="quote-box">
+      <Quote quote={quote} author={author} />
+    </div>
+  );
+};
 
 export default QuoteBox;
